7b3891708b775f25c4971dcf139cfb5077c46851
Tylor Schafer <tylorschafer@gmail.com>
2019-12-17 09:19:42 -0700

    Setup word watch class

---
+++ docs/wordWatch.js
@@ -0,0 +1,7 @@
+const fetch = require('node-fetch')
+
+class WordWatch {
+  
+}
+
+module.exports = WordWatch
--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,10 +1,10 @@
 import { expect } from "chai"
 import sinon from "sinon"
 
-describe("Word watch functions", () => {
-  describe("", () => {
-    it("", () => {
-    })
-  })
-})
+// describe("Word watch functions", () => {
+//   describe("", () => {
+//     it("", () => {
+//     })
+//   })
+// })
 
