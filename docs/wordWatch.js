0807aeaf505a034f22c61b38f2a9fda589271891
Tylor Schafer <tylorschafer@gmail.com>
2019-12-17 11:50:44 -0700

    Added some functionality

--- docs/wordWatch.js
+++ docs/wordWatch.js
@@ -1,7 +1,12 @@
 const fetch = require('node-fetch')
 
 class WordWatch {
-  
+  async getTopWord () {
+    const topWord = await fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+      .then(result => result)
+      .catch(error => error)
+    return topWord
+  }
 }
 
 module.exports = WordWatch
